f416fd2ce36fdfc49c6bd1274666fe1f41af52d9
Mehran Kamalifard <m.kamalifard@hoshmand.local>
2023-01-23 09:27:21 +0330

    Initial Commit

---
+++ EasyCrypto/EasyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCryptoApp.swift
+//  EasyCrypto
+//
+//  Created by Mehran Kamalifard on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

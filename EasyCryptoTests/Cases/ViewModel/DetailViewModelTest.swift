a232a9262e7fc05bf99385e1dfd2e831e73a3ce4
Mehran Kamalifard <mehran.kmlf@icloud.com>
2023-05-02 23:27:17 +0330

    Adde unit test classes

---
+++ EasyCryptoTests/Cases/ViewModel/DetailViewModelTest.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewModelTest.swift
+//  EasyCryptoTests
+//
+//  Created by Mehran on 2/7/1402 AP.
+//
+
+import XCTest
+import Combine
+@testable import EasyCrypto
+
+final class DetailViewModelTest: XCTestCase {
+    
+    private var viewModelToTest: DetailViewModel!
+
+    override func setUp()  {
+        
+    }
+    
+    override func tearDown() {
+            
+        super.tearDown()
+    }
+}
